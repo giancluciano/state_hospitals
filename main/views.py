0c1b26854ae7a19c4544c33d53aff023a0c86951
gian <giancluciano@gmail.com>
2023-12-05 00:37:06 -0300

    adding cache

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.views.generic import TemplateView
 from main.models import Hospital
 from rest_framework import viewsets
 from main.serializers import HospitalSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 
 class HospitalsView(TemplateView):
@@ -21,6 +23,10 @@ class HospitalViewSet(viewsets.ModelViewSet):
     A simple ViewSet for listing hospitals.
     """
     serializer_class = HospitalSerializer
+
+    @method_decorator(cache_page(60*60*2))
+    def list(self, *args, **kwargs):
+        return super().list(*args, **kwargs)
 
     def get_queryset(self):
         state = self.kwargs.get('state')
